3700043bb72eca8d94d4bc8e7b13c01579f0bc54
Piotr Kluch (extern) <piotr.kluch.extern@blueid.net>
2022-04-20 16:18:34 +0200

    Modify nohup cmd

--- home/docker/connect-to-docker.sh
+++ home/docker/connect-to-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2 &
+nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 &
