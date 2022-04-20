2fad2f19336eef30c4d5b3760dcdba106f1168a7
Piotr Kluch (extern) <piotr.kluch.extern@blueid.net>
2022-04-20 16:26:49 +0200

    Remove /home directory altogether

--- home/docker/connect-to-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 &
--- home/docker/wait-15-seconds.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-timeout 15 sh -c "until docker info; do echo .; sleep 1; done"
