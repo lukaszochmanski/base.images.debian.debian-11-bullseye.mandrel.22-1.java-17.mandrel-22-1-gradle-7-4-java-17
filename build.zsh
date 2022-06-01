db5c50a1a753263a2ce94349de031db01ae3e193
Lukasz Ochmanski (extern) <lukasz.ochmanski.extern@blueid.net>
2022-06-01 09:19:28 +0200

    graalvm-11-gradle-7-4:1.1.15

--- build.zsh
+++ build.zsh
@@ -3,27 +3,22 @@
 #login
 cd $AWS/base/images/debian/debian-11-bullseye/gradle/gradle-7-4/graalvm-ce-17/graalvm-17-gradle-7-4 || exit
 HOST=964010022385.dkr.ecr.eu-central-1.amazonaws.com
-GROUP_ID=base/images/debian/debian-11-bullseye/gradle/gradle-7-4/graalvm-ce-17
-ARTIFACT_ID=graalvm-17-gradle-7-4
-IMAGE_TAG=1.0.1
-cat README.md.tpl | sed s,"###CI_COMMIT_TAG###","$IMAGE_TAG", > README.md.tpl2
-TODAY=`date`
-cat README.md.tpl2 | sed s,"###TIMESTAMP###","$TODAY", > README.md
-rm README.md.tpl2
-
-if [[ "$IMAGE_TAG" == *-SNAPSHOT ]]
-then
-  echo "no tag"
-else
-  git add . && git commit -m 'Install docker daemon'
-  git tag -a $IMAGE_TAG -m 'Install docker daemon'
-  git push --follow-tags
-fi
+GROUP_ID=$(gradle -q printGroup)
+ARTIFACT_ID=$(gradle -q printRootProjectNameRaw)
+IMAGE_TAG=$(gradle -q printVersion)
 
 docker build -t "$HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG-test .
 docker run --rm -ti --privileged --entrypoint /bin/bash "$HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG-test
 docker image rm "$HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG-test
 echo "https://eu-central-1.console.aws.amazon.com/codesuite/codebuild/964010022385/projects/base-images-debian-debian-11-bullseye-gradle-gradle-7-4-graalvm-ce-17-graalvm-17-gradle-7-4/details?region=eu-central-1"
 echo
-echo "docker build -t $HOST/$GROUP_ID/$ARTIFACT_ID:$IMAGE_TAG ."
-echo "docker push $HOST/$GROUP_ID/$ARTIFACT_ID:$IMAGE_TAG"
+
+if [[ "$IMAGE_TAG" == "$LATEST_TAG" ]];
+then
+  export DOCKER_REPOSITORY=''
+else
+  export DOCKER_REPOSITORY='snapshots/'
+fi
+
+echo "docker build -t $HOST/$DOCKER_REPOSITORY$GROUP_ID/$ARTIFACT_ID:$IMAGE_TAG ."
+echo "docker push $HOST/$DOCKER_REPOSITORY$GROUP_ID/$ARTIFACT_ID:$IMAGE_TAG"
---
+++ bump.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+#login
+cd $AWS/base/images/debian/debian-11-bullseye/gradle/gradle-7-4/graalvm-ce-17/graalvm-17-gradle-7-4 || exit
+tagMessage='graalvm-11-gradle-7-4:1.1.15'
+export tagMessage
+
+git add . && git commit -m $tagMessage
+
+
+PROJECT_VERSION=$(gradle -q printVersion)
+if [[ "$PROJECT_VERSION" == *-SNAPSHOT ]]
+then
+  IMAGE_TAG=${PROJECT_VERSION:0:-9}
+else
+  IMAGE_TAG=$PROJECT_VERSION
+fi
+
+cat README.md.tpl | sed s,"###CI_COMMIT_TAG###","$IMAGE_TAG", > README.md.tpl2
+TODAY=`date`
+cat README.md.tpl2 | sed s,"###TIMESTAMP###","$TODAY", > README.md
+rm README.md.tpl2
+git add . && git commit -m "Update README.md to version: $IMAGE_TAG"
+
+gradle tag -PbumpComponent=patch -Prelease
+LATEST_VER=$(gradle -q printVersion)
+git tag -d $LATEST_VER
+git tag -a $LATEST_VER -m $tagMessage
+git push --follow-tags
+git tags
+gradle printVersion
+
