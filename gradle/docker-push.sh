a071eeed8b8603dd1e4e31a13f64461f3562eab8
Lukasz Ochmanski (extern) <lukasz.ochmanski.extern@blueid.net>
2022-06-27 15:01:59 +0200

    bugFix in ./gradle/docker-push.sh

--- gradle/docker-push.sh
+++ gradle/docker-push.sh
@@ -3,12 +3,12 @@
 PROJECT_VERSION=$(gradle -q printVersion)
 if [[ "$PROJECT_VERSION" == *-SNAPSHOT ]]
 then
+    echo "INFO: No git tag found for this commit."
+    echo "INFO: You see this message because no snapshot repository was set up for this project."
+    echo "INFO: Therefore, pushing snapshots is not possible at this moment."
+    echo "INFO: This build will be stopped here."
+    echo "INFO: to avoid this message, execute: ./bump.zsh locally."
+else
   gradle dockerPush
   /home/gradle/summary.sh
-else
-  echo "INFO: No git tag found for this commit."
-  echo "INFO: You see this message because no snapshot repository was set up for this project."
-  echo "INFO: Therefore, pushing snapshots is not possible at this moment."
-  echo "INFO: This build will be stopped here."
-  echo "INFO: to avoid this message, execute: ./bump.zsh locally."
 fi
