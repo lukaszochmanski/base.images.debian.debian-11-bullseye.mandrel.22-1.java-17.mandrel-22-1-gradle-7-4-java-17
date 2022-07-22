60b04176a705216c9ceb749abfce49fce4069805
Lukasz Ochmanski (extern) <lukasz.ochmanski.extern@blueid.net>
2022-07-22 15:21:07 +0200

    use stable version vegardit/graalvm-11-maven:1.0.6

--- scripts/home/aws/ecr/login.sh
+++ scripts/home/aws/ecr/login.sh
@@ -2,5 +2,20 @@
 
 set +x
 set -e
-echo -n "ECR "
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $HOST
+
+discardWarning () {
+  if grep -Fq "Login Succeeded" output.tmp_file ;
+  then
+    echo
+    echo "ECR Login Succeeded"
+  else
+    echo
+    echo "ECR Login Failed"
+    cat output.tmp_file
+    exit 1
+  fi
+  rm output.tmp_file
+}
+
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $HOST > output.tmp_file
+discardWarning
