afeef7df322f77fb0178120856b9b40eaefcf042
Lukasz Ochmanski (extern) <lukasz.ochmanski.extern@blueid.net>
2022-07-22 16:09:56 +0200

    redirect both output (stdout) and errors (stderr) to output.tmp_file

--- scripts/home/aws/ecr/login.sh
+++ scripts/home/aws/ecr/login.sh
@@ -6,10 +6,8 @@ set -e
 discardWarning () {
   if grep -Fq "Login Succeeded" output.tmp_file ;
   then
-    echo
     echo "ECR Login Succeeded"
   else
-    echo
     echo "ECR Login Failed"
     cat output.tmp_file
     exit 1
@@ -17,5 +15,5 @@ discardWarning () {
   rm output.tmp_file
 }
 
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $HOST 2> output.tmp_file
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $HOST > output.tmp_file 2>&1
 discardWarning
